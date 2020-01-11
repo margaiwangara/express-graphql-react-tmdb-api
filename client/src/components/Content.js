cf4e3f96e19d59ae25a385384a5ad12163971db7
margaiwangara <margaiwangara@gmail.com>
2020-01-11 18:03:31 +0300

    styled cards for displaying content

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -4,16 +4,16 @@ import { Grid, Row, Col } from "react-flexbox-grid";
 export default function Content() {
   return (
     <div className="content">
-      <h3 className="h3">Large Title</h3>
-      <Grid fluid>
-        <Row>
-          <Col xs={12}>
-            <div className="card">
-              <p>This is my card</p>
-            </div>
-          </Col>
-        </Row>
-      </Grid>
+      <h3 className="h3 text-white">New Releases</h3>
+      <div className="inner-row">
+        <div className="card">
+          <div className="card-top">image area</div>
+          <div className="card-bottom">
+            <h5 className="h5 text-white">Home Alone</h5>
+            <h6 className="h6 text-white-o">Adventure, Comedy</h6>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
