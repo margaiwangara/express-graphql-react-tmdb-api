06dde27bcd593c235ce1c5276e09447ffc0e8419
margaiwangara <margaiwangara@gmail.com>
2020-01-12 12:06:28 +0300

    created functions to convert runtime to hrs and mins and display all genres

--- client/src/components/MovieDetails.js
+++ client/src/components/MovieDetails.js
@@ -7,21 +7,30 @@ import Moment from "react-moment";
 import poster from "../assets/pic6.jpg";
 import { MOVIE_DETAILS_QUERY } from "../graphql/queries";
 
-function movieRuntime(runtimeInMins) {
-  // check if divisible by 60
-  if (runtimeInMins % 60 === 0) {
-    return `${runtimeInMins / 60}hrs`;
+function formatGenre(genreList) {
+  let container = "";
+
+  for (let i = 0; i < genreList.length; i++) {
+    container +=
+      i != genreList.length - 1 ? `${genreList[i].name}, ` : genreList[i].name;
   }
 
-  const hrs = Math.floor(runtimeInMins / 60);
-  const mins = runtimeInMins % 60;
-  // else get modulus then get remainder and floor
-  return hrs > 1
-    ? hrs + "hr"
-    : hrs + "hrs" + " " + mins > 1
-    ? mins + "min"
-    : mins + "mins";
+  return container;
 }
+
+function formatRuntime(runTimeInMins) {
+  // 60 mins = 1hr
+  const rtHrs = Math.floor(runTimeInMins / 60);
+  const rtMins = runTimeInMins % 60;
+
+  // check if rthrs == 0
+  if (rtHrs == 0) return `${runTimeInMins}mins`;
+
+  let container = rtHrs > 1 ? `${rtHrs}hrs ` : `${rtHrs}hr `;
+  container += rtMins == 0 ? "" : rtMins > 1 ? `${rtMins}min` : `${rtMins}mins`;
+  return container;
+}
+
 export default function MovieDetails(props) {
   let { movie_id } = props.match.params;
   movie_id = parseInt(movie_id);
@@ -38,19 +47,21 @@ export default function MovieDetails(props) {
     genres,
     release_date,
     runtime,
-    overview
+    overview,
+    poster_path
   } = data.movie;
 
+  let imagePath = `https://image.tmdb.org/t/p/w500/${poster_path}`;
   return (
     <div className="details">
       <div className="details-top">
         <figure>
-          <img src={poster} alt="poster-image" />
+          <img src={imagePath} alt="poster-image" />
         </figure>
         <h3 className="h3">{original_title}</h3>
         <div className="content-group">
           <h5 className="h5">Category</h5>
-          <p>Adventure, Science Fiction</p>
+          <p>{formatGenre(genres)}</p>
         </div>
         <div className="content-group">
           <h5 className="h5">Release Date</h5>
@@ -62,7 +73,7 @@ export default function MovieDetails(props) {
         </div>
         <div className="content-group">
           <h5 className="h5">Runtime</h5>
-          <p>{runtime}</p>
+          <p>{formatRuntime(runtime)}</p>
         </div>
         <div className="content-group">
           <h5 className="h5">Homepage</h5>
