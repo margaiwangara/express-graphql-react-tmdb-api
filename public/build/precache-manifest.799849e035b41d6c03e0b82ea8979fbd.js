4376aed570b70f6da4d8d237b145c2571cda9f41
margaiwangara <margaiwangara@gmail.com>
2020-01-12 13:32:08 +0300

    modified gitignore to include public folder

---
+++ public/build/precache-manifest.799849e035b41d6c03e0b82ea8979fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e4e543f75f48488ce5ffd51860db07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f5e9fd086e72583737",
+    "url": "/static/css/main.cc0bcdef.chunk.css"
+  },
+  {
+    "revision": "39f71455fe29f2c98dbe",
+    "url": "/static/js/2.089642c5.chunk.js"
+  },
+  {
+    "revision": "b95ac56ec0dfc8a73bc74705cd57dbe0",
+    "url": "/static/js/2.089642c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83f5e9fd086e72583737",
+    "url": "/static/js/main.b2de7c55.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
